22336b1327e3dae2fb141a2fb5db71697622fd30
Alexey Rybak <alexey.rybak@gmail.com>
2023-08-02 17:57:16 +0300

    removing the annoying warning (incompatible pointer type)

--- blitz_debug.c
+++ blitz_debug.c
@@ -5,14 +5,14 @@ static int dump_array_values(void *pDest, int num_args, va_list args, zend_hash_
 
 void blitz_dump_ht(HashTable *ht, int depth) {
     php_printf("%*carray(%d) {\n", depth * 2, ' ', zend_hash_num_elements(ht));
-    zend_hash_apply_with_arguments(ht, dump_array_values, 1, depth + 1);
+    zend_hash_apply_with_arguments(ht, (apply_func_args_t) dump_array_values, 1, depth + 1);
     php_printf("%*c}\n", depth * 2, ' ');
 }
 
 static void dump_value(zval *zv, int depth) {
     if (Z_TYPE_P(zv) == IS_ARRAY) {
         php_printf("%*carray(%d) {\n", depth * 2, ' ', zend_hash_num_elements(Z_ARRVAL_P(zv)));
-        zend_hash_apply_with_arguments(Z_ARRVAL_P(zv), dump_array_values, 1, depth + 1);
+        zend_hash_apply_with_arguments(Z_ARRVAL_P(zv), (apply_func_args_t) dump_array_values, 1, depth + 1);
         php_printf("%*c}\n", depth * 2, ' ');
     } else {
         php_printf("%*c%Z\n", depth * 2, ' ', zv);
